4aacca63836efb1249bbef08d37465759479205c
Pablo Alecio <paleciop@gmail.com>
2017-08-27 21:52:17 -0500

    First commit.

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+});
+
+app.listen(7070, function () {
+  console.log('Nova Server started.')
+});
