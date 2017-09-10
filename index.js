636f4fc4835c6c67036d7b167bb33d0d8fa3792d
Pablo Alecio <paleciop@gmail.com>
2017-09-10 00:52:41 -0500

    Refactored the server a bit. Moved the server functionality to lib/server.js, index.js is now only an entry point. Improved logging (a lil bit). Implemented 404 and 500 errors. Now the cpe doesn't get instantiated each time there's a request.

--- index.js
+++ index.js
@@ -1,63 +1,19 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const nova = require('/Users/palecio/Documents/nova/nova-core');
-const C = require('./lib/constants');
+#!/usr/bin/env node
+/**
+ * @fileOverview Nova Server.
+ * @author <a href="mailto:paleciop@gmail.com">Pablo Alecio</a>
+ */
+'use strict';
 
-const app = express();
-const router = express.Router();
-const templatingEngine = nova.templatingEngine;
+const server = require('./lib/server');
 
-const DEFAULT_NOVA_PORT = 9001;
-
-const executeContextProcessors = async function executeContextProcessors(
-  executionContext,
-  contentModel,
-  contextProcessorPaths
-) {
-  const cpPaths = Array.isArray(contextProcessorPaths)
-    ? contextProcessorPaths
-    : [contextProcessorPaths];
-  const cpe = await nova.fetchContextProcessorEngine({
-    paths: cpPaths
+if (require.main === module) {
+  // If called through the CLI
+  server.start({
+    contextProcessorPaths: 'examples/contextprocessors',
+    baseContentModel: { initial: 'INIT!' }
   });
-  return cpe.execute(executionContext, contentModel);
-};
-
-const start = function start(configuration = {}) {
-  const {
-    port = DEFAULT_NOVA_PORT,
-    baseURLPath = '/api',
-    contextProcessorPaths = [],
-    baseContentModel = {}
-  } = configuration;
-
-  const handleRequest = function handleRequest(request, response) {
-    const requestContentModel =
-      (request.body && request.body.contentModel) || {};
-    executeContextProcessors(
-      { path: request.path, request },
-      Object.assign({}, baseContentModel, requestContentModel),
-      contextProcessorPaths
-    )
-      .then(contentModel => {
-        const html = request.body && request.body.html;
-        response.send(
-          html
-            ? templatingEngine.compileTemplate(html, contentModel)
-            : contentModel
-        );
-      })
-      .catch(e => console.error(e));
-  };
-
-  router.use(bodyParser.json());
-  router.all(C.ASTERISK, handleRequest);
-  app.use(baseURLPath, router);
-  app.listen(port, function() {
-    console.log(`Nova Server started on port ${port}`);
-  });
-};
-
-module.exports = {
-  start
-};
+} else {
+  // Required as a module
+  module.exports = server;
+}
