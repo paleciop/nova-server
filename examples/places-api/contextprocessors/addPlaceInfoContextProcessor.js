448232dfec232d14d021d6979e579178c418bd97
Pablo Alecio <paleciop@gmail.com>
2017-09-13 20:16:36 -0500

    Added simple cache.

--- examples/places-api/contextprocessors/addPlaceInfoContextProcessor.js
+++ examples/places-api/contextprocessors/addPlaceInfoContextProcessor.js
@@ -4,6 +4,8 @@ const http = require('axios');
 const contextProcessor = require('/Users/palecio/Documents/nova/nova-core')
   .pathAwareContextProcessor;
 
+const cache = {};
+
 module.exports = contextProcessor.extend({
   patterns: ['/places/info/*', '/places/info', '/places/everything', '/places/everything/*'],
   process(executionContext, contentModel) {
@@ -13,7 +15,15 @@ module.exports = contextProcessor.extend({
     const place = contentModel.place;
     if (appId && baseUrl && place) {
       const url = `${baseUrl}?appid=${appId}&input=${place}&output=json`;
-      return http.get(url).then(response => contentModel.info = response.data).catch(e=>console.error(e));
+      if (cache[url]) {
+        return cache[url];
+      } else {
+        return http.get(url).then(response => {
+          cache[url] = response.data;
+          contentModel.info = response.data;
+        }).catch(e=>console.error(e));
+      }
+
     }
   }
 });
