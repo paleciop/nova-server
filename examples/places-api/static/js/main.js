896bfa5b8025001e4e542944dc5f2d709b6433ef
Pablo Alecio <paleciop@gmail.com>
2017-09-13 20:16:48 -0500

    Added Vuejs

--- examples/places-api/static/js/main.js
+++ examples/places-api/static/js/main.js
@@ -1,5 +1,23 @@
 'use strict';
 
-axios.post('http://localhost:9001/api/places/everything/Volcan Acatenango').then(response => {
-  document.getElementById('content').innerHTML = JSON.stringify(response.data, null, 2);
+
+
+const data = {
+  place: '',
+  title: ''
+};
+
+var app = new Vue({
+  el: '#app',
+  methods: {
+    setTitle: function (event) {
+      event.preventDefault();
+      this.title = this.place;
+      axios.post('http://localhost:9001/api/places/everything/'+this.place).then(response => {
+        document.getElementById('content').innerHTML = JSON.stringify(response.data, null, 2);
+      });
+    }
+  },
+  data
 });
+
